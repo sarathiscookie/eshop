436909c7d2cb93072f0dc762963adbafa2f22c89
Sarath <sarathiscookie@hotmail.com>
2017-02-02 01:12:30 +0530

    Clean Up: Some clean up in each user blade page.

--- resources/views/seller/index.blade.php
+++ resources/views/seller/index.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Seller home</div>
 
                 <div class="panel-body">
-                    You are logged in seller!
+                    You are logged in as seller!
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,11 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        @if (Auth::user()->hasRole('buyer'))
+                            <a href="{{ url('/buyer/home') }}">Home</a>
+                        @elseif(Auth::user()->hasRole('seller'))
+                            <a href="{{ url('/seller/home') }}">Home</a>
+                        @endif
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
